cd2d592cc24d9093edca06a24b3fc3c42276e65c
Melvin Laguren <melvin@laguren.net>
2024-07-09 21:49:06 -0700

    3 implement rules engine to process data comparison of dataframes (#8)
    * #3: Initial Commit Using Durable Rules As A Rules Engine, with some basic data comparison rules added.
    
    ---------
    
    Co-authored-by: Melvin Laguren <melvin.laguren@ecoatm.com>

--- libs/connector_factory.py
+++ libs/connector_factory.py
@@ -4,8 +4,32 @@ from libs.csv_connector import CSVConnector
 from libs.postgres_connector import PostgresConnector
 
 class ConnectorFactory:
+    """
+    A factory class for creating data connectors.
+
+    This class provides a static method `get_connector` that creates and returns a data connector based on the provided configuration.
+
+    The configuration should be a dictionary that contains a 'type' key, which specifies the type of connector to create ('csv' or 'postgres'), and other keys that provide the necessary information for creating the connector.
+
+    If the 'type' key is 'csv', the configuration should also contain a 'location' key that specifies the location of the CSV file.
+
+    If the 'type' key is 'postgres', the configuration should also contain a 'connection' key that is a dictionary with the following keys: 'host', 'password', 'port', 'dbname', and 'query'.
+    """
+
     @staticmethod
     def get_connector(config):
+        """
+        Creates and returns a data connector based on the provided configuration.
+
+        Parameters:
+        config (dict): The configuration for creating the data connector.
+
+        Returns:
+        DataConnector: The created data connector.
+
+        Raises:
+        ValueError: If the 'type' key in the configuration is not 'csv' or 'postgres'.
+        """
         print(config)
         connector_type = config['type']
 
@@ -17,6 +41,6 @@ class ConnectorFactory:
                                      config['connection']['host'],
                                      config['connection']['port'],
                                      config['connection']['dbname'],
-                                     config['query'])
+                                     config['connection']['query'])
         else:
             raise ValueError(f"Unsupported connector type: {connector_type}")
--- tests/test_check_keys.py
+++ tests/test_check_keys.py
@@ -6,10 +6,13 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from libs.check_keys import check_keys_in_list
 
-
 def test_valid_file():
     """
     Test case where all required keys are present.
+
+    This test case checks if the function `check_keys_in_list` correctly identifies when all required keys are present in a dictionary.
+
+    The dictionary `test_case` contains all the required keys. The function `check_keys_in_list` is expected to return True and None, indicating that all required keys are present and no keys are missing.
     """
     # Example test case with all required keys
     test_case = {'name': 'Simple Test',
@@ -28,6 +31,10 @@ def test_valid_file():
 def test_missing_keys():
     """
     Test case where some required keys are missing.
+
+    This test case checks if the function `check_keys_in_list` correctly identifies when some required keys are missing from a dictionary.
+
+    The dictionary `test_case` is missing the key 'name'. The function `check_keys_in_list` is expected to return False and the set {'name'}, indicating that not all required keys are present and the key 'name' is missing.
     """
     # Example test case missing some keys
     test_case = {'source': {'type': 'csv', 'location': './data.csv'},
