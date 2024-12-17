9589b5db8a4fed6104bd49b85fc6b5de51b150f1
Paramveer Bright <154287722+paramveerbright1982@users.noreply.github.com>
2024-12-17 16:58:45 -0500

    Implemented CSV generation feature (#35)

---
+++ libs/constants.py
@@ -0,0 +1,8 @@
+import os
+# ===============================================
+# Please Dont make changes to the below constants
+# ===============================================
+ROOT_FOLDER = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+OUTPUT_FOLDER = os.path.join(ROOT_FOLDER, "outputs")
+
+# INPUT_FOLDER = os.path.join(ROOT_FOLDER, "inputs")
